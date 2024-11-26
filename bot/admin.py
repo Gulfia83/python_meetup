e73a77b90b38ced57dab382b377bcf3f36f29679
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-11-26 11:30:31 +0300

    Update admin.py

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Donate, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("tg_id", "name", "company", "position", "status")
+
+
+@admin.register(Donate)
+class DonateAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "amount",
+        "donated_at",
+    )
