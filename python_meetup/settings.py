6fa4e45492e5924efac57704c3c12efc77721223
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-11-26 11:30:55 +0300

    Add gt_token to settingss.py

--- python_meetup/settings.py
+++ python_meetup/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1"])
-
+TG_BOT_TOKEN = env.str('TG_BOT_TOKEN')
 
 # Application definition
 
