2b1c348e473335504e6dafc0fb5701c34e339d62
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-27 23:38:47 +0300

    feat(settings): add static_root

--- python_meetup/settings.py
+++ python_meetup/settings.py
@@ -1,7 +1,6 @@
-from email.policy import default
 from environs import Env
 from pathlib import Path
-
+import os
 env = Env()
 env.read_env()
 
@@ -108,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
