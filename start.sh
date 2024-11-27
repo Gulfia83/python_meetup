07344365e3ac96b543070875f674181b1b009a40
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-28 00:42:28 +0300

    refactor(deploy_script)

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@
 if [ "$1" == "web" ]; then
   python manage.py migrate --noinput
 
+    python manage.py createsuperuser --noinput \
+    --username root \
+    --email admin@example.com \
+    --password root
+
   exec python manage.py runserver 0.0.0.0:8000
 elif [ "$1" == "bot" ]; then
   exec python tg_bot.py
