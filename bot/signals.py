0d576642addd8ad2c0616bfc925d2aadf3c71e04
Макаров Степан Александрович <st.mkv03@gmail.com>
2024-11-27 19:47:34 +0300

    refactor(signals)

--- bot/signals.py
+++ bot/signals.py
@@ -4,6 +4,5 @@ from .models import Letters
 
 @receiver(post_save, sender=Letters)
 def send_newsletter_on_save(sender, instance, created, **kwargs):
-    print("wegfwrgwrg")
     if created and instance.sent_at is None:
         instance.send_to_all_users()
